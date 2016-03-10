dec3373ff71cc084d02a94ed9aba9873a7d37517
Josh Miller <notjoshmiller@gmail.com>
2016-03-10 02:20:34 +0000

    Stylize

--- src/cljs/sequences/views.cljs
+++ src/cljs/sequences/views.cljs
@@ -26,7 +26,7 @@
     (syn/gain 0.05)))
 
 (def melody
-  (->> (melody/phrase (cycle [0.5]) (take 500 iseries))
+  (->> (melody/phrase (cycle [0.5]) (take 1000 iseries))
        (melody/all :instrument synth)))
 
 (def track
@@ -35,14 +35,14 @@
      (melody/where :pitch scale/G)))
 
 (defn setup []
-  (q/frame-rate 30)
+  (q/frame-rate 60)
   (q/no-loop)
   (let [notes track
         dots (map #(vec [(/ (* (:pitch %) (:time %)) 20) (* (:time %) 2)]) notes)]
     {:dots dots
      :notes notes}))
 
-(def speed 0.0002)
+(def speed 0.0001)
 
 (defn move [dot]
   (let [[r a] dot]
@@ -56,20 +56,23 @@
    (+ (/ (q/height) 2) (* r (q/cos a)))])
 
 (defn draw-state [state]
-  (q/background 255)
+  (q/background 0)
   (q/fill 0)
   (let [dots (:dots state)
         notes (:notes state)
-        sync (re-frame/subscribe [:sync])
-        playing? (re-frame/subscribe [:playing?])
+        sync (re-frame/subscribe [:sync]) 
+        playing? (re-frame/subscribe [:playing?]) 
         relative-time (-> (Date.now) (- @sync) (mod (* 1000 (melody/duration notes))) (/ 1000))
-        marked (map #(if (and @playing? (<= (/ (first (rest %)) 10) relative-time)) (conj % :played) %) dots)]
+        marked (filter #(and @playing? (<= (/ (first (rest %)) 8) relative-time)) dots)]
+    #_(if (= (count dots) (count marked)) (q/no-loop))
     (loop [curr (first marked)
            tail (rest marked)
            prev nil]
       (let [[x y] (dot->coord curr)
-            paint (if (= (last curr) :played) (q/color (q/random 0 100) (q/random 100 200) (q/random 100 255)) 255)]
+            paint (q/color (q/random 0 100) (q/random 100 200) (q/random 100 255))]
         (q/stroke paint)
+        (q/stroke-weight 2)
+        (q/stroke-join :round)
         (q/fill paint)
         (when prev
           (let [[x2 y2] (dot->coord prev)]
@@ -102,10 +105,10 @@
           (q/with-sketch (q/get-sketch-by-id "canvas")
             (if @playing? (q/start-loop) (q/no-loop))))
         [:main
-          [:h1.title "Sequences"]
-          [:button {:on-click #(re-frame/dispatch (if @playing? [:stop] [:start track]))}
-            (if @playing? "Stop" "Start")]
-          [canvas]
+          [:section
+            [:button {:on-click #(re-frame/dispatch (if @playing? [:stop] [:start track]))}
+              (if @playing? "Stop" "Infinitize")]
+            [canvas]]
         ]
     )
 ))
